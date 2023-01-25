d139f55d35e8daceef9b6ac73771c8a0291c68d3
diegosena7 <diego.tjdosantos@gmail.com>
2023-01-24 23:30:04 -0300

    add data partida com horario

--- src/main/java/com/br/dsena/agendadejogos/model/Partida.java
+++ src/main/java/com/br/dsena/agendadejogos/model/Partida.java
@@ -2,16 +2,13 @@ package com.br.dsena.agendadejogos.model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.persistence.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Entity
 @Data
 public class Partida {
+
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     @Column(name = "id", nullable = false)
@@ -22,12 +19,7 @@ public class Partida {
     private String timeAdversario;
 
 
-//    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy")
-    @DateTimeFormat(pattern = "dd/MM/yyyy HH:mm")
-    private String dataPartida;
+    @JsonFormat(shape=JsonFormat.Shape.STRING, pattern="dd/MM/yyyy HH:mm")
+    private Date dataPartida;
 
-    public String setDataPartida(Date dataPartida) throws ParseException {
-        SimpleDateFormat fmt = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-        return this.dataPartida = fmt.format(dataPartida);
-    }
 }
